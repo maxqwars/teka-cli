c9457829b5b9aa39f351ff14e697ac8f4eb7ce1a
Maxim Maximenko <maxqwars@gmail.com>
2022-08-29 03:05:54 +0700

    1.0.1

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,13 +1,13 @@
-import { Types, Constants } from '@maxqwars/metaform'
+import { Types, Constants } from "@maxqwars/metaform";
 
-const baseUrl = "api.anilibria.tv"
-const timeout = 6000
+const baseUrl = "api.anilibria.tv";
+const timeout = 6000;
 
 const sharedConfig: Types.MetaModuleOptions = {
   timeout,
   baseUrl,
   useHttps: true,
-  version: Constants.API_VERSION.V2
-}
+  version: Constants.API_VERSION.V2,
+};
 
-export default sharedConfig
+export default sharedConfig;
